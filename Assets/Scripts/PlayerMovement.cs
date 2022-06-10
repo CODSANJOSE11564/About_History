f7713b15f8e9abfd055da33deea45c2a86ee5b7c
Ferchus <giovfpollo115@gmail.com>
2022-06-09 21:04:08 -0500

    yes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody = default;
+    [SerializeField] private Animator animator = default;
+    [SerializeField] private float _xvalue = default;
+    [SerializeField] private float _speed = default;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _xvalue = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
+
+    private void Movement()
+    {
+        _rigidbody.velocity = new Vector3(_xvalue * _speed, 0, 0);
+        animator.SetInteger("Velocidad", Mathf.FloorToInt(_xvalue));
+    }
+}
